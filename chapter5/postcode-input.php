cdb75bb707b938cfb27537194307b79c46c987df
tanakaryo <ch5cxcxxh@gmail.com>
2020-08-28 16:59:10 +0900

    prctice 正規表現

--- chapter5/postcode-input.php
+++ chapter5/postcode-input.php
@@ -1,3 +1,7 @@
 <?php require '../header.php'; ?>
-
+<p>7桁の郵便番号をハイフンなしで入力してください。</p>
+<form action="postcode-output.php" method="post">
+    <input type="text" name="postcode">
+    <p><input type="submit" value="確定"></p>
+</form>
 <?php require '../footer.php'; ?>
--- chapter5/postcode-input2.php
+++ chapter5/postcode-input2.php
@@ -1,3 +1,7 @@
 <?php require '../header.php'; ?>
-
+<p>7桁の郵便番号をハイフンありで入力してください。</p>
+<form action="postcode-output2.php" method="post">
+    <input type="text" name="postcode">
+    <p><input type="submit" value="確定"></p>
+</form>
 <?php require '../footer.php'; ?>
