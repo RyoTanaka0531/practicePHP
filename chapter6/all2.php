7c5fd3679061dab4c17c452e90d6b62d313735bf
tanakaryo <ch5cxcxxh@gmail.com>
2020-08-29 22:26:53 +0900

    use htmlspecialchars

--- chapter6/all2.php
+++ chapter6/all2.php
@@ -1,3 +1,16 @@
 <?php require '../header.php'; ?>
-
+<?php
+$pdo=new PDO('mysql:
+                    host=localhost;
+                    dbname=shop;
+                    charset=utf8',
+                    'staff', 'password');
+    foreach ($pdo -> query('SELECT * FROM product') as $row){
+        echo '<p>';
+        echo $row['id'], ':';
+        echo $row['name'], ':';
+        echo $row['price'], ':';
+        echo '</p>';
+    }
+?>
 <?php require '../footer.php'; ?>
--- chapter6/all3.php
+++ chapter6/all3.php
@@ -1,3 +1,12 @@
 <?php require '../header.php'; ?>
-
+<?php
+$pdo = new PDO('mysql:
+                    host=localhost;
+                    dbname=shop;
+                    charset=utf8',
+                    'staff', 'password');
+    foreach ($pdo -> query('SELECT * FROM product') as $row){
+        echo "<p>$row[id]:$row[name]:$row[price]</p>";
+    }
+?>
 <?php require '../footer.php'; ?>
