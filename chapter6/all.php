a91eff2c72e010bf2b8301c314678c9d7ce26372
tanakaryo <ch5cxcxxh@gmail.com>
2020-08-29 20:29:52 +0900

    use PDO

--- chapter6/all.php
+++ chapter6/all.php
@@ -1,3 +1,9 @@
 <?php require '../header.php'; ?>
-
+<?php
+$pdo=new PDO('mysql:
+                    host = localhost;
+                    dbname = shop;
+                    charset = utf8',
+                'staff', 'password');
+?>
 <?php require '../footer.php'; ?>
