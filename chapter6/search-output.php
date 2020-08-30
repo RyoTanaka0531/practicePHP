14686f957c194a5cd44b8d349e84891a63322338
tanakaryo <ch5cxcxxh@gmail.com>
2020-08-30 21:54:14 +0900

    practice wildcard

--- chapter6/search-output.php
+++ chapter6/search-output.php
@@ -1,3 +1,31 @@
 <?php require '../header.php'; ?>
+<table>
+    <tr>
+        <th>商品番号</th>
+        <th>商品名</th>
+        <th>商品価格</th>
+    </tr>
+    <?php
+    $pdo =new PDO('mysql:
+                        host=localhost;
+                        dbname=shop;
+                        charset=utf8',
+                        'staff', 'password');
+    // prepareメソッドはSQL文を実行する準備
+    // 「?」の部分には後から好きな値を設定することができる。
+    $sql = $pdo -> prepare('SELECT * FROM product WHERE name=?');
+    // prepareメソッドに引数として渡したSQL文を実行するためexecuteメソッドを使用。
+    // 引数には、SQL文の中の「?」部分に設定する値を配列にして渡す。
+    $sql -> execute([$_REQUEST['keyword']]);
+    foreach ($sql as $row){
+        echo '<tr>';
+        echo '<td>', $row['id'], '</td>';
+        echo '<td>', $row['name'], '</td>';
+        echo '<td>', $row['price'], '</td>';
+        echo '</tr>';
+        echo "\n";
+    }
+    ?>
+</table>
 
 <?php require '../footer.php'; ?>
--- chapter6/search-output2.php
+++ chapter6/search-output2.php
@@ -1,3 +1,26 @@
 <?php require '../header.php'; ?>
-
+<table>
+    <tr>
+        <th>商品番号</th>
+        <th>商品名</th>
+        <th>商品価格</th>
+    </tr>
+    <?php
+    $pdo = new PDO('mysql:
+                        host=localhost;
+                        dbname=shop;
+                        charset=utf8',
+                        'staff', 'password');
+    $sql = $pdo -> prepare('SELECT * FROM product WHERE name LIKE ?');
+    $sql -> execute(['%'.$_REQUEST['keyword'].'%']);
+    foreach ($sql as $row){
+        echo '<tr>';
+        echo '<td>', $row['id'], '</td>';
+        echo '<td>', $row['name'], '</td>';
+        echo '<td>', $row['price'], '</td>';
+        echo '</tr>';
+        echo "\n";
+    }
+    ?>
+</table>
 <?php require '../footer.php'; ?>
