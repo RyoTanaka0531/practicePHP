6f297a3ad459d31bf34ff495aa08fb4307c1de0f
tanakaryo <ch5cxcxxh@gmail.com>
2020-08-31 12:45:09 +0900

    SQL delete

--- chapter6/delete-input.php
+++ chapter6/delete-input.php
@@ -1,3 +1,27 @@
 <?php require '../header.php'; ?>
-
+<table>
+    <tr>
+        <td>商品番号</td>
+        <td>商品名</td>
+        <td>商品価格</td>
+    </tr>
+    <?php
+    $pdo = new PDO('mysql:
+                        host=localhost;
+                        dbname=shop;
+                        charset=utf8',
+                        'staff', 'password');
+    foreach($pdo -> query('SELECT * FROM product') as $row){
+        echo '<tr>';
+        echo '<td>', $row['id'], '</td>';
+        echo '<td>', $row['name'], '</td>';
+        echo '<td>', $row['price'], '</td>';
+        echo '<td>';
+        echo '<a href="delete-output.php?id=', $row['id'], '">削除</a>';
+        echo '</td>';
+        echo '</tr>';
+        echo "\n";
+    }
+    ?>
+</table>
 <?php require '../footer.php'; ?>
--- chapter6/delete-output.php
+++ chapter6/delete-output.php
@@ -1,3 +1,15 @@
 <?php require '../header.php'; ?>
-
+<?php
+$pdo = new PDO('mysql:
+                    host=localhost;
+                    dbname=shop;
+                    charset=utf8',
+                    'staff', 'password');
+$sql = $pdo ->prepare('DELETE FROM product WHERE id=?');
+if ($sql -> execute([$_REQUEST['id']])){
+    echo '削除に成功しました。';
+} else{
+    echo '削除に失敗しました。';
+}
+?>
 <?php require '../footer.php'; ?>
