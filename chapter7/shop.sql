d2d18b8177b728c49a12a9a01a6920359eb5cfd8
tanakaryo <ch5cxcxxh@gmail.com>
2020-09-02 14:16:16 +0900

    practice session

--- chapter7/shop.sql
+++ chapter7/shop.sql
@@ -4,39 +4,39 @@ grant all on shop.* to 'staff'@'localhost' identified by 'password';
 use shop;
 
 create table product (
-	id int auto_increment primary key, 
-	name varchar(200) not null, 
+	id int auto_increment primary key,
+	name varchar(200) not null,
 	price int not null
 );
 
 create table customer (
-	id int auto_increment primary key, 
-	name varchar(100) not null, 
-	address varchar(200) not null, 
-	login varchar(100) not null unique, 
+	id int auto_increment primary key,
+	name varchar(100) not null,
+	address varchar(200) not null,
+	login varchar(100) not null unique,
 	password varchar(100) not null
 );
 
 create table purchase (
-	id int not null primary key, 
-	customer_id int not null, 
+	id int not null primary key,
+	customer_id int not null,
 	foreign key(customer_id) references customer(id)
 );
 
 create table purchase_detail (
-	purchase_id int not null, 
-	product_id int not null, 
-	count int not null, 
-	primary key(purchase_id, product_id), 
-	foreign key(purchase_id) references purchase(id), 
+	purchase_id int not null,
+	product_id int not null,
+	count int not null,
+	primary key(purchase_id, product_id),
+	foreign key(purchase_id) references purchase(id),
 	foreign key(product_id) references product(id)
 );
 
 create table favorite (
-	customer_id int not null, 
-	product_id int not null, 
-	primary key(customer_id, product_id), 
-	foreign key(customer_id) references customer(id), 
+	customer_id int not null,
+	product_id int not null,
+	primary key(customer_id, product_id),
+	foreign key(customer_id) references customer(id),
 	foreign key(product_id) references product(id)
 );
 
