8e1df1bfff2a4029c8e7a17b83f01308dee71ae4
tanakaryo <ch5cxcxxh@gmail.com>
2020-09-05 11:19:10 +0900

    add favorite

---
+++ chapter7/favorite-insert.php
@@ -0,0 +1,20 @@
+<?php session_start();?>
+<?php require '../header.php';?>
+<?php require 'menu.php';?>
+<?php
+if (isset($_SESSION['customer'])){
+    $pdo=new PDO('mysql:
+                        host=localhost;
+                        dbname=shop;
+                        charset=utf8',
+                        'staff', 'password');
+    $sql=$pdo -> prepare('INSERT INTO favorite VALUES(?,?)');
+    $sql->execute([$_SESSION['customer']['id'], $_REQUEST['id']]);
+    echo 'お気に入り商品を追加しました。';
+    echo '<hr>';
+    require 'favorite.php';
+} else{
+    echo 'お気に入りに商品を追加するには、ログインしてください。';
+}
+?>
+<?php require '../footer.php';?>
